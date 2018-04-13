0d7ee2affa74ac533b411232fb90fac0ac070071
ubuntu <ubuntu@localhost.localdomain>
2018-04-13 21:41:38 +0800

    first commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '6c8cc24ca789b8750a6984989464f01a',
+			hash: '6e671a53c84fbdddf8d54173d939f047c9c3f707ac21c0effeee4a42e959b57b',
+			user_created_timestamp: 1523625211587
+		}
+	]
+})
